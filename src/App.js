4457bf2d8ee0c09f039f3e41663f2617690331db
juancasanchez6 <jcsanchezr04@educarex.es>
2023-11-22 11:03:08 +0100

    Update-Weather

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import './App.css';
-import Navbar from './componentes/Navbar';
+import React, { useEffect } from "react";
+import "./App.css";
+
 
 function App() {
+  
+
   return (
     <div className="App">
-      <Navbar />
+      
     </div>
   );
 }
